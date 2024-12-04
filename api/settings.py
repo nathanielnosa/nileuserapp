6e36e26effba705c0477313f0019bd130c14a74d
nathaniel nosa <nathanielnarth@gmail.com>
2024-12-04 15:00:47 +0100

    creating model fields

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#hj^r%%ir^z1)+_!w+_1765m@dfxff5r$nqwhyk+i4*#@vtq)-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1:8000']
 
 
 # Application definition
--- userapps/models.py
+++ userapps/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+GENDER =(
+    ('M','Male'),
+    ('F','Female'),
+)
 class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    fullname = models.CharField(max_length=255)
+    phone = models.CharField(max_length=50)
+    gender = models.CharField(max_length=10, choices=GENDER)
+    profile_pix = models.ImageField(upload_to='profile', blank=True,null=True)
 
     def __str__(self):
-        pass
+        return self.fullname
