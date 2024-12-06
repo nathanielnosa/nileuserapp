e97e6b85a51db0e265c22e3190ac3a3fbab1cba4
nathaniel nosa <nathanielnarth@gmail.com>
2024-12-06 13:26:21 +0100

    registration view and image settings

--- api/urls.py
+++ api/urls.py
@@ -2,7 +2,11 @@
 from django.contrib import admin
 from django.urls import path,include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('nileuser/', admin.site.urls),
     path('nile/',include('userapps.urls'))
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
--- userapps/views.py
+++ userapps/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import serializers
 
-# Create your views here.
+from . serializers import UserSerializer,ProfileSerailizer,RegistrationSerializer
+from . models import Profile
+from django.contrib.auth.models import User
+
+# registration
+class RegistrationView(APIView):
+    def post(self,request):
+        try:
+            serializer = RegistrationSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            return Response({'error':str(e)}, status = status.HTTP_INTERNAL_SERVER_ERROR)
+        
